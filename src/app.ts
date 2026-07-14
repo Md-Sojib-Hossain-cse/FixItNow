e241211832ee345a7aee8208f88db68cf4c332df
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2026-07-14 13:56:26 +0600

    project setup done and root route added

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { type Application, type Request, type Response } from "express"
+import cors from "cors"
+import config from "./config"
+import cookieParser from "cookie-parser"
+
+const app : Application = express()
+
+app.use(cors({
+    origin : config.app_url
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
+
+app.get("/" , async(req : Request , res : Response) => {
+    res.json({
+        message : "Welcome to FixItNow!"
+    })
+})
+
+export default app;
